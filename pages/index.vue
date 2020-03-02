205088f6a83100fbbf7b1aabb8815a1c5c42037c
jalofernandez <jalofernandez@gmail.com>
2020-03-02 11:31:44 +0100

    ✨all Favicons variants: OS + browsers in Home page

--- pages/index.vue
+++ pages/index.vue
@@ -306,9 +306,22 @@ export default {
         { rel: 'dns-prefetch', href: '//peluqueriacanessa.com/' },
         { rel: 'dns-prefetch', href: '//ecomerzpro.net/' },
         { rel: 'dns-prefetch', href: '//capaball.com/app/#/login/' },
-        { rel: 'dns-prefetch', href: '//jalofernandez.github.io/lfcabogados/' }
+        { rel: 'dns-prefetch', href: '//jalofernandez.github.io/lfcabogados/' },
         // (custom) Favicons
-        // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/favicon/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: "32x32", href: '/favicon/favicon-32x32.png' },
+        { rel: 'apple-touch-icon', sizes: "57x57", href: '/favicon/app-icon-57x57.png' },
+        { rel: 'apple-touch-icon', sizes: "60x60", href: '/favicon/app-icon-60x60.png' },
+        { rel: 'apple-touch-icon', sizes: "72x72", href: '/favicon/app-icon-72x72.png' },
+        { rel: 'apple-touch-icon', sizes: "76x76", href: '/favicon/app-icon-76x76.png' },
+        { rel: 'apple-touch-icon', sizes: "114x114", href: '/favicon/app-icon-114x114.png' },
+        { rel: 'apple-touch-icon', sizes: "120x120", href: '/favicon/app-icon-120x120.png' },
+        { rel: 'apple-touch-icon', sizes: "144x144", href: '/favicon/app-icon-144x144.png' },
+        { rel: 'apple-touch-icon', sizes: "152x152", href: '/favicon/app-icon-152x152.png' },
+        { rel: 'apple-touch-icon', sizes: "180x180", href: '/favicon/app-icon-180x180.png' },
+        // Browsers Tabs
+        { rel: 'mask-icon', color: "#333333", href: '/favicon/safari-pinned-tab.svg' }
       ]
     }
   },
