bccec897d57d583b044b94f3e3ef712bd3747567
jalofernandez <jalofernandez@gmail.com>
2021-04-11 15:40:30 +0200

    ♻️  Aside´s design updated in all Businesses

--- components/TheAside.vue
+++ components/TheAside.vue
@@ -1,15 +1,21 @@
 <template>
   <nav class="aside-menu with-anim">
-    <img
-      class="aside-business-logo"
-      :src="require(`~/assets/negocios/${business.id}/${business.id}-logo-color.png`)"
-      :alt="`Logotipo de ${business.name} en ${business.place}`"
-      :title="`Logotipo de ${business.name} en ${business.place}`"
-      v-if="business.logo"
-    >
-    <h2 class="aside-business-name" v-else>{{ business.name }}</h2>
-    <ul>
-      <li v-for="(menu, index) in business.menus" :key="index">
+    <div class="aside-header">
+      <img
+        class="aside-business-logo"
+        :src="require(`~/assets/negocios/${business.id}/${business.id}-logo-color.png`)"
+        :alt="`Logotipo de ${business.name} en ${business.place}`"
+        :title="`Logotipo de ${business.name} en ${business.place}`"
+        v-if="business.logo"
+      >
+      <h2 class="aside-business-name" v-else>{{ business.name }}</h2>
+    </div>
+    <ul class="aside-sections">
+      <li
+        class="aside-section"
+        v-for="(menu, index) in business.menus" 
+        :key="index"
+      >
         <a
           href="#"
           class="aside-menu-link"
