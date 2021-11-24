d007395bc8f2ed6898df4f5a0a413c0671a5538a
jalofernandez <jalofernandez@gmail.com>
2021-11-24 10:21:12 +0100

    ⬆️  Project version updated to 1.2.5

--- store/landing.js
+++ store/landing.js
@@ -22,7 +22,7 @@ export const state = () => ({
     whatsapp: 34696682791,
     footer: '<span class="has-text-weight-medium">Digitaliza tu</span> <b class="has-text-success">negocio</b>',
     dataImage: 'haz-tu-negocio-digital-metadata-card.jpg',
-    version: '1.2.4',
+    version: '1.2.5',
     schema: {
       '@context': 'http://schema.org',
       '@type': 'LocalBusiness',
